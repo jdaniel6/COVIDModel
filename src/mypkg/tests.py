95abb7ef3d2131f8217c255b8e838a46ea690d06
Jeffrey Daniel <jdaniel6@stevens.edu>
2021-05-01 18:33:24 -0400

    Updates the previous version to be more complete and correct
    Several code and logic changes were made to slightly improve the program, and to provide a more accurate simulation. Screenshots of a simulation are included in this build, reflecting a real-world scenario. No known bugs remain at this time, but the code may be cleaned up in future builds.

--- src/mypkg/tests.py
+++ src/mypkg/tests.py
@@ -1,35 +1,42 @@
 from main import Graph
-import math, random,scipy,sys,os
+import math, random,scipy,sys,os,time
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+time1=time.perf_counter()
+
+#future stuff
 import subprocess
 subprocess.call('', shell=True)
 from termcolor import colored
 import colorama
 colorama.init()
 os.system('color')
+
+
 #inititialise a model connection
-pop = 100
+pop = 10000
+prev=[]
 g={}
 temp=""
 pos=[0]*pop
 def init(social_distancing):
-	global g, pop, temp, pos
+	global g, pop, temp, pos, prev
 	pos=[0]*pop
+	prev=[0]*pop
 	for i in range(0,pop):
 		temp=str(i)
 		g[temp]=[]
 	if social_distancing==True:
-		x1,x2=0,2
+		x1,x2=5,10
 	else:
-		x1,x2=3,7
+		x1,x2=10,20
 	for i in range(0,random.randint(x1,x2)):
 		for i in g.values():
 			i.append(str(random.randint(0,pop-1)))
-	for i in range(0,4):
+	for i in range(0,40):
 		pos[random.randint(0,pop-1)]=1
-	for i in range(0,6):
+	for i in range(0,60):
 		pos[random.randint(0,pop-1)]=2
 
 """
@@ -44,20 +51,24 @@ start the covid pandemic
 
 x2=2
 
-print(len(pos))
-print(len(g))	
+#print(len(pos))
+#print(len(g))	
 def drawnext():
 	global color_map
 	color_map=[]
 	global pos,x2
-	print(pos)
+	#print(pos)
 	for x in pos:
 		if x==0:
 			color_map.append("b")
 		elif x==1:
 			color_map.append("g")
-		else:
+		elif x==2:
 			color_map.append("r")
+		elif x==3:
+			color_map.append("black")
+		elif x==4:
+			color_map.append('pink')
 	plt.subplot(2,3,x2)
 	plt.title("At %i months" %(4*(x2-2)))
 	x2+=1
@@ -66,49 +77,68 @@ def drawnext():
 """
 every 12 days, infect a new person
 if a person is infected, all people connected to them will get infected with probability COVID, whether they are symptomatic or not
+people die with probability probd
+dead people cannot transmit disease (set as 3)
+0: uninfected
+1: infected asymptomatic
+2: infected symptomatic
+3: dead
+4: recovered and immune
+#https://coronavirus.jhu.edu/data/mortality
 """
-count1=[0]*50
-count2=[0]*50
+count1=[0]*50 #number of cases in a masked society
+count2=[0]*50 #number of cases in a maskless society
+count3=[0]*50 #deaths in a masked society
+count4=[0]*50 #deaths in a maskless society
 def infect(masking):
-	global count1,count2
+	global count1,count2,count3,count4
+	count=0
+	probd=25
 	if masking==True:
-		prob=45731
+		probinf=45731
 	else:
-		prob=76218
-	for i in range(0,50):
-		indices=[j for j,x in enumerate(pos) if (x==1 or x==2)]
-		for j in indices:
-			for k in list(g.values())[j]: #range(0,len(
-				if(random.randint(0,1000000)<prob and pos[j]!=2):
+		probinf=76218
+	for i in range(0,50): #counter
+		indices=[j for j,x in enumerate(pos) if (x==1 or x==2)] #gives the source people who are positive
+		for j in indices: #for all these people,
+			for k in list(g.values())[j]: #for every list of connected people to the source person(j), take individually (k),
+				if(random.randint(0,1000000)<probinf and (pos[int(k)]!=2 and pos[int(k)]!=3 and pos[int(k)]!=4)): #probability of getting infected asymp
 					#g[str(j)][k]=1
 					pos[int(k)]=1
-				elif(random.randint(0,1000000)<prob and pos[j]!=1):
+				elif(random.randint(0,1000000)<probinf and (pos[int(k)]!=1 and pos[int(k)]!=3 and pos[int(k)]!=4)): #probability of getting infected symp
 					#g[str(j)][k]=2
 					pos[int(k)]=2
+			#trial 
+			if((prev[j]==1 or prev[j]==2)): #if the source person was infected previously
+				if(random.randint(0,1000)<probd): #they may die
+					pos[j]=3
+				else: #or they may recover
+					pos[j]=4
+			elif(prev[j]==0): #or prev[j]==3 or prev[j]==4): #if they were healthy, dead or recovered
+				if(pos[j]==1 or pos[j]==2): #if they are currently infected
+					prev[j]=pos[j]
+			elif(prev[j]==3):
+				pos[j]=3
+			elif(prev[j]==4):
+				pos[j]=4
+					
+				#if(prev[i]==1 or prev[i]==2):
+				#	pos[
 		for x in pos:
-			if (x==1 or x==2):
+			if (x==1 or x==2 or x==3 or x==4):
 				if masking==True:
 					count1[i]+=1
 				else:
 					count2[i]+=1
-	
+			if x==3:
+				count+=1				
+			if masking==True:
+				count3[i]=count
+			else:
+				count4[i]=count
+		count=0
 		if(i%10==0):
 			drawnext()
-				
-	
-"""
-every 12 days, deaths are updated
-people die with probability death
-dead people cannot transmit disease (set as -1 or remove from the graph?)
-"""
-def death():
-	pass
-	
-	
-
-#print(graph)
-
-
 
 #plot the network
 def fig():
@@ -130,33 +160,64 @@ def fig():
 
 text1='Blue: Healthy\n'#colored('Blue: Healthy\n','blue')
 text2='Green: Infected but Asymptomatic\n'#colored('Green: Infected but Asymptomatic\n','green')
-text3='Red: Infected and Symptomatic'#colored('Red: Infected and Symptomatic','red')
+text3='Red: Infected and Symptomatic\n'#colored('Red: Infected and Symptomatic','red')
+text4='Black: Deaths\nPink: Recovered'
 
 init(False)
 fig2=plt.figure()
 fig2.suptitle("Network with no social distancing or masking")
-fig2.text(0.02,0.02,("Population of %i \nLegend: \n" %pop +text1 +text2 +text3))
+fig2.text(0.02,0.02,("Population of %i \nLegend: \n" %pop +text1 +text2 +text3+text4))
 fig()
 infect(False)
-
+print("Total number of infected cases after no social distancing or masking: %i" %count2[-1])
+print("Total number of deaths after no social distancing or masking: %i"  %count4[-1])
+print("Infection rate with no social distancing or masking: %.02f" %((count2[-1]/pop)*100)+"%")
+print("Mortality rate with no social distancing or masking: %.02f" %((count4[-1]/pop)*100)+"%")
 
 
 init(True)
 fig2=plt.figure()
 fig2.suptitle("Network with social distancing and masking")
-fig2.text(0.02,0.02,("Population of %i \nLegend: \n" %pop +text1 +text2 +text3))
+fig2.text(0.02,0.02,("Population of %i \nLegend: \n" %pop +text1 +text2 +text3+text4))
 fig()
 x2=2
 infect(True)
 
+print("Total number of infected cases after social distancing and masking: %i"  %count1[-1])
+print("Total number of deaths after social distancing and masking: %i" %count3[-1])
+print("Infection rate with social distancing and masking: %.02f" %((count1[-1]/pop)*100)+"%")
+print("Mortality rate with social distancing and masking: %.02f" %((count3[-1]/pop)*100)+"%")
+
 
 fig3=plt.figure()
-fig3.suptitle("Total number of infected people")
+fig3.suptitle("Total number of cases and deaths")
 labels=['0 months','4 months', '8 months','12 months','16 months']
-plt.plot(list(range(40)),count2[:40])
 plt.plot(list(range(40)),count1[:40])
+plt.plot(list(range(40)),count2[:40])
+plt.plot(list(range(40)),count3[:40])
+plt.plot(list(range(40)),count4[:40])
 plt.xticks([0,10,20,30,40],labels)
-plt.yticks([0,20,40,60,80,100],[0,int(0.2*pop),int(0.4*pop),int(0.6*pop),int(0.8*pop),pop])
-plt.legend(['No social distancing or masking','Good social distancing and masking'])
+#plt.yticks([0,20,40,60,80,100],[0,int(0.2*pop),int(0.4*pop),int(0.6*pop),int(0.8*pop),pop])
+plt.legend(['Cases after good social distancing and masking','Cases after no social distancing or masking','Deaths after social distancing and masking','Deaths with no social distancing or masking'])
+
+fig4=plt.figure()
+fig4.suptitle("Increase in cases and deaths")
+count11,count21,count31,count41=[0]*40,[0]*40,[0]*40,[0]*40
+for i in range(1,40):
+	count11[i]=count1[i]-count1[i-1]
+	count21[i]=count2[i]-count2[i-1]
+	count31[i]=count3[i]-count3[i-1]
+	count41[i]=count4[i]-count4[i-1]
+plt.plot(list(range(40)),count11[:40])
+plt.plot(list(range(40)),count21[:40])
+plt.plot(list(range(40)),count31[:40])
+plt.plot(list(range(40)),count41[:40])
+plt.xticks([0,10,20,30,40],labels)
+#plt.yticks([0,20,40,60,80,100],[0,int(0.2*pop),int(0.4*pop),int(0.6*pop),int(0.8*pop),pop])
+plt.legend(['Cases after good social distancing and masking','Cases after no social distancing or masking','Deaths after social distancing and masking','Deaths with no social distancing or masking'])
+time2=time.perf_counter()
+print("Time taken for simulating %i population: " %pop +str(time2-time1)+" seconds")
+#print(count3)
+#print(count4)
 plt.show()
 
